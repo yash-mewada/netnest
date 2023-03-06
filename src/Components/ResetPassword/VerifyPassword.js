4049f665c8af68fc0d995d29afac9745dcb981be
yash-mewada <yashmewada27@gmail.com>
2023-03-06 15:24:46 +0530

    created verify password page

--- src/Components/ResetPassword/VerifyPassword.js
+++ src/Components/ResetPassword/VerifyPassword.js
@@ -1,14 +1,17 @@
+import "./VerifyPassword.css";
 function VerifyPassword() {
   return (
     <div className="verify-parent-container">
       <div className="verify-container">
         <h1>Reset Password</h1>
         <p>Create your new password</p>
-        <div>
-          <input type="password" placeholder="i.e Example@gmail.com"></input>
-          <input type="password" placeholder="i.e Example@gmail.com"></input>
-          <buton className="otp-next">Next</buton>
-        </div>
+        <form>
+          <div className="form-control">
+            <input type="password" placeholder="New password"></input>
+            <input type="password" placeholder="Type it again"></input>
+            <buton className="done">Done</buton>
+          </div>
+        </form>
       </div>
     </div>
   );
